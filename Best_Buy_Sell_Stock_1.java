2333499528e4923d5a3a100fc9cde26773bf2f60
Gouri Sabale <sabalegouri1996@gmail.com>
2024-11-21 22:58:09 -0600

    Create Best_Buy_Sell_Stock_1.java

---
+++ Best_Buy_Sell_Stock_1.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+
+        int maxprofit=0;
+        int min=prices[0];
+
+        for(int i=1;i<prices.length;i++)
+        {
+            if(prices[i]-min > maxprofit)
+            {
+                maxprofit=prices[i]-min;
+            }
+            min=Math.min(min,prices[i]);
+        }
+        
+        return maxprofit;
+    }
+}
